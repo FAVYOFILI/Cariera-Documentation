99a21a31b3587a13caf5fcc078cbae6961d0e547
Lucky C. Ofili <lcofili2000@gmail.com>
2025-07-20 20:52:05 +0100

    Cariera Page

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,45 +1,135 @@
-import Gnologo from "./assets/GNOlogo.png"
-GIT ADD .
+// import Gnologo from "./assets/GNOlogo.png"
 
-const Header = () => {
-
+// const Header = () => {
 
 
 
 
-    return (
-      <header className="bg-white px-20 py-6 flex justify-between items-center">
-        <div>
-          <img src={Gnologo} className=" h-[40px]" />
-        </div>
 
-         <div className="h-[40px] w-[40px] lg:hidden bg-[#47c494] flex justify-center items-center cursor-pointer rounded-[5px]">
-       </div>
+//     return (
+//       <header className="bg-white px-20 py-6 flex justify-between items-center">
+//         <div>
+//           <img src={Gnologo} className=" h-[40px]" />
+//         </div>
 
-        <div className=" lg:flex gap-6 justify-center items-center mr-8 hidden">
-          <nav className="flex items-center"> 
-            <ul className="flex gap-6 text-black text-[14px]">
-              <li className="text-[#8e709c] ">HOME</li>
+//          <div className="h-[40px] w-[40px] lg:hidden bg-[#47c494] flex justify-center items-center cursor-pointer rounded-[5px]">
+//        </div>
+
+//         <div className=" lg:flex gap-6 justify-center items-center mr-8 hidden">
+//           <nav className="flex items-center"> 
+//             <ul className="flex gap-6 text-black text-[14px]">
+//               <li className="text-[#8e709c] ">HOME</li>
 
              
                
-              <li className="text-[rgb(94, 94, 94)]">DEMO</li>
-              <li>SUPPORT</li>
-              <li>THEMES</li>
-              <li>FB GROUP</li>
-                </ul>
+//               <li className="text-[rgb(94, 94, 94)]">DEMO</li>
+//               <li>SUPPORT</li>
+//               <li>THEMES</li>
+//               <li>FB GROUP</li>
+//                 </ul>
                 
               
-          </nav>
+//           </nav>
 
-          <button className="px-[28px] py-[10px] flex rounded-md justify-center items-center text-white bg-[#24a3a4]">
-            Buy Cariera
-          </button>
+//           <button className="px-[28px] py-[10px] flex rounded-md justify-center items-center text-white bg-[#24a3a4]">
+//             Buy Cariera
+//           </button>
+//         </div>
+
+
+//       </header>
+//     );
+// }
+
+// export default Header
+
+
+import { useState } from "react";
+import Gnologo from "./assets/GNOlogo.png";
+
+const Header = () => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  return (
+    <header className="bg-white px-20 py-6 flex justify-between items-center">
+      <div>
+        <img src={Gnologo} className="h-[40px]" alt="GNO Logo" />
+      </div>
+
+     
+      <div
+        className="h-[40px] w-[40px] lg:hidden bg-[#47c494] flex justify-center items-center cursor-pointer rounded-[5px] relative"
+        onMouseEnter={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+      >
+       
+        <div className="space-y-1.5">
+          <span className="block w-6 h-0.5 bg-white"></span>
+          <span className="block w-6 h-0.5 bg-white"></span>
+          <span className="block w-6 h-0.5 bg-white"></span>
         </div>
 
+      
+        {isHovering && (
+          <div className="absolute top-full right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-[#47c494] hover:text-white"
+            >
+              HOME
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-[#47c494] hover:text-white"
+            >
+              DEMO
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-[#47c494] hover:text-white"
+            >
+              SUPPORT
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-[#47c494] hover:text-white"
+            >
+              THEMES
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-[#47c494] hover:text-white"
+            >
+              FB GROUP
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-white bg-[#24a3a4] rounded mx-2 my-1 text-center"
+            >
+              Buy Cariera
+            </a>
+          </div>
+        )}
+      </div>
 
-      </header>
-    );
-}
+      {/* Desktop Navigation */}
+      <div className="lg:flex gap-6 justify-center items-center mr-8 hidden">
+        <nav className="flex items-center">
+          <ul className="flex gap-6 text-black text-[14px]">
+            <li className="text-[#8e709c]">HOME</li>
+            <li className="text-[rgb(94, 94, 94)]">DEMO</li>
+            <li>SUPPORT</li>
+            <li>THEMES</li>
+            <li>FB GROUP</li>
+          </ul>
+        </nav>
 
-export default Header
+        <button className="px-[28px] py-[10px] flex rounded-md justify-center items-center text-white bg-[#24a3a4]">
+          Buy Cariera
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
